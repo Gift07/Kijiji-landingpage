32deb47e98cdf451c7a11f9a85817c973746ab46
gift07 <giftedwards9@gmail.com>
2023-06-10 13:24:50 +0300

    jun 10 13;24

--- src/Components/Home/Vision.jsx
+++ src/Components/Home/Vision.jsx
@@ -19,6 +19,94 @@ const Vision = () => {
                 </h1>
               </div>
             </div>
+            {/* words introduction */}
+            <div className="absolute -right-12 top-24 z-10">
+              <h1 className="text-2xl font-lora text-main font-medium py-3">
+                Alexa
+              </h1>
+              <p className="text-main text-sm font-medium">
+                Talk to our multilingual
+                <br /> support team
+              </p>
+            </div>
+            {/* Tanzanian Flag */}
+            <div className="absolute left-0 top-16 z-10">
+              <div className="h-12 bg-gray bg-opacity-30 backdrop-blur-md w-44 rounded-full shadow-md border border-main border-opacity-30 flex items-center px-2 gap-x-2">
+                <div>
+                  <img
+                    src="tzflag.png"
+                    alt="flag1"
+                    className="h-10 w-10 rounded-full object-cover"
+                  />
+                </div>
+                <div className="h-full w-[110px] py-1">
+                  <div className="flex items-center justify-between">
+                    <h1 className="uppercase text-sm font-medium font-lora text-main">
+                      Tzs
+                    </h1>
+                    <h1 className="uppercase text-sm font-medium font-lora text-main">
+                      20M
+                    </h1>
+                  </div>
+                  <div className="h-[.5px] w-full bg-main bg-opacity-30" />
+                  <h1 className="text-xs font-light text-main">
+                    instant transfer
+                  </h1>
+                </div>
+              </div>
+            </div>
+            {/* american flag */}
+            <div className="absolute left-0 bottom-24 z-10">
+              <div className="h-12 bg-gray bg-opacity-30 backdrop-blur-md w-44 rounded-full shadow-md border border-main border-opacity-30 flex items-center px-2 gap-x-2">
+                <div>
+                  <img
+                    src="usflag.jpg"
+                    alt="flag1"
+                    className="h-10 w-10 rounded-full object-cover"
+                  />
+                </div>
+                <div className="h-full w-[110px] py-1">
+                  <div className="flex items-center justify-between">
+                    <h1 className="uppercase text-sm font-medium font-lora text-main">
+                      USD
+                    </h1>
+                    <h1 className="uppercase text-sm font-medium font-lora text-main">
+                      20K
+                    </h1>
+                  </div>
+                  <div className="h-[.5px] w-full bg-main bg-opacity-30" />
+                  <h1 className="text-xs font-light text-main">
+                    instant transfer
+                  </h1>
+                </div>
+              </div>
+            </div>
+            {/* euro flag */}
+            <div className="absolute right-0 bottom-32 z-10">
+              <div className="h-12 bg-gray bg-opacity-30 backdrop-blur-md w-44 rounded-full shadow-md border border-main border-opacity-30 flex items-center px-2 gap-x-2">
+                <div>
+                  <img
+                    src="euflag.png"
+                    alt="flag1"
+                    className="h-10 w-10 rounded-full object-cover"
+                  />
+                </div>
+                <div className="h-full w-[110px] py-1">
+                  <div className="flex items-center justify-between">
+                    <h1 className="uppercase text-sm font-medium font-lora text-main">
+                      Eur
+                    </h1>
+                    <h1 className="uppercase text-sm font-medium font-lora text-main">
+                      20k
+                    </h1>
+                  </div>
+                  <div className="h-[.5px] w-full bg-main bg-opacity-30" />
+                  <h1 className="text-xs font-light text-main">
+                    instant transfer
+                  </h1>
+                </div>
+              </div>
+            </div>
             <div className="bg-love h-[34rem] w-[34rem] rounded-full flex relative  justify-center overflow-hidden">
               <img
                 src="miguel.png"
